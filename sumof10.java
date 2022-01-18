8f5e09409beff67f4a0f0ba4cf0f694be52f6279
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2022-01-18 14:22:38 +0530

    Added sumof10

---
+++ sumof10.java
@@ -0,0 +1,13 @@
+package firstClass;
+
+public class assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;				// Sum of first 10 natural numbers
+		int sum = (n*(n+1))/2;
+		
+		System.out.println("Sum of first 10 numbers = " + sum);
+	}
+
+}
